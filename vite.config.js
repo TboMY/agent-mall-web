6fab556bfa5926c7e3b9ccbff2b3d1dba5decc6f
TboMY <2322507003@qq.com>
2025-09-23 17:52:39 +0800

    git test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// gittest
+// gittest
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  //gittest
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
