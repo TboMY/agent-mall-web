c67e0e42aa2329021a07942d641aed0031220837
cj <2322507003@qq.com>
2025-09-23 18:08:46 +0800

    local git test 4

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-//ytuhjkl;
-//ujikl;sf
+// 3456789398492
+// 98-09090980-
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  //gittest
-  
+  //gittest123123
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
