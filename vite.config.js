5391c40b262bfc7b347c9ed39f6439979e1bef02
TboMY <2322507003@qq.com>
2025-09-23 18:08:20 +0800

    remote git test 4

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-///111111
-////123123
+//ytuhjkl;
+//ujikl;sf
 export default defineConfig({
   plugins: [
     vue(),
